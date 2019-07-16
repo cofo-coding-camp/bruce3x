01b84891fa4845cf34256d6cfe78b92f3cd95342
bruce3x <im.brucezz@gmail.com>
2019-07-16 13:02:04 +0800

    solved #160

---
+++ solutions/src/test/kotlin/com/bruce3x/leetcode/160.kt
@@ -0,0 +1,28 @@
+@file:Suppress("PackageDirectoryMismatch")
+
+package com.bruce3x.leetcode._160
+
+import com.bruce3x.leetcode.ListNode
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class SolutionTest {
+    @Test
+    fun getIntersectionNode() {
+        val solution = Solution()
+
+        val s = ListNode.create(1, 8, 4, 5)
+        val s1 = ListNode(4).apply { next = s }
+        val s2 = ListNode(5).apply { next = ListNode(0).apply { next = s } }
+        assertEquals(s, solution.getIntersectionNode(s1, s2))
+
+        assertEquals(
+            null,
+            solution.getIntersectionNode(
+                ListNode.create(2, 6, 4),
+                ListNode.create(1, 5)
+            )?.value
+        )
+    }
+}

8035e32f9576ca9b94d7cc66088129b8bb53b127
bruce3x <im.brucezz@gmail.com>
2019-07-18 14:04:52 +0800

    optimize #583

--- solutions/src/main/kotlin/com/bruce3x/leetcode/583.kt
+++ solutions/src/main/kotlin/com/bruce3x/leetcode/583.kt
@@ -30,50 +30,38 @@
 
 package com.bruce3x.leetcode._583
 
-import kotlin.math.max
+import kotlin.math.min
 
 /**
  * LCS problem
  *
  * C[i,j] =
- *    0                                 ( i = 0 or j = 0 )
- *    C[i-1, j-1]                       (  X[i] == Y[j]  )
- *    max( C[i, j-1], C[i-1, j] )       (  X[i] == Y[j]  )
+ *    j                                 ( i = 0 )
+ *    i                                 ( j = 0 )
+ *    C[i-1, j-1]                       ( X[i] == Y[j] )
+ *    min( C[i, j-1], C[i-1, j] )       ( X[i] != Y[j] )
  */
 class Solution {
     fun minDistance(word1: String?, word2: String?): Int {
         val s1 = word1.orEmpty()
         val s2 = word2.orEmpty()
-        val len = lcsLengthIterative(s1, s2)
-        return s1.length + s2.length - 2 * len
-    }
 
-    private fun lcsLengthRecursively(s1: String, s2: String): Int =
-        lcsLengthRecursively(s1, s2, s1.length - 1, s2.length - 1)
-
-    private fun lcsLengthRecursively(s1: String, s2: String, i: Int, j: Int): Int {
-        return when {
-            i < 0 || j < 0 -> 0
-            s1[i] == s2[j] -> lcsLengthRecursively(s1, s2, i - 1, j - 1) + 1
-            else -> max(
-                lcsLengthRecursively(s1, s2, i - 1, j),
-                lcsLengthRecursively(s1, s2, i, j - 1)
-            )
-        }
-    }
-
-
-    private fun lcsLengthIterative(s1: String, s2: String): Int {
         val dp = Array(s1.length + 1) { IntArray(s2.length + 1) }
-        var i = 1
 
+        for (i in 0..s1.length) dp[i][0] = i
+        for (j in 0..s2.length) dp[0][j] = j
+
+        var i = 1
         while (i <= s1.length) {
             var j = 1
             while (j <= s2.length) {
                 dp[i][j] = if (s1[i - 1] == s2[j - 1]) {
-                    dp[i - 1][j - 1] + 1
+                    dp[i - 1][j - 1]
                 } else {
-                    max(dp[i - 1][j], dp[i][j - 1])
+                    1 + min(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    )
                 }
 
                 j++
@@ -83,5 +71,4 @@ class Solution {
 
         return dp[s1.length][s2.length]
     }
-
 }

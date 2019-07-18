ee3b4610c4d6b3731ed10028a1889722656786be
bruce3x <im.brucezz@gmail.com>
2019-07-18 17:12:11 +0800

    tree node

---
+++ solutions/src/main/kotlin/com/bruce3x/leetcode/TreeNode.kt
@@ -0,0 +1,50 @@
+@file:Suppress("EqualsOrHashCode")
+
+package com.bruce3x.leetcode
+
+import java.util.*
+
+class TreeNode(val value: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is TreeNode) return false
+        return this.value == other.value && this.left == other.left && this.right == other.right
+    }
+
+    override fun toString(): String {
+        val queue: Queue<TreeNode?> = LinkedList()
+        val nodes = mutableListOf<TreeNode?>()
+        queue.add(this)
+        while (queue.isNotEmpty()) {
+            val node = queue.remove()
+            nodes.add(node)
+
+            node?.run {
+                queue.add(left)
+                queue.add(right)
+            }
+        }
+
+        return "[" + nodes.joinToString { it?.value.toString() } + "]"
+    }
+
+    companion object {
+        fun create(vararg values: Int?): TreeNode {
+            val rootValue = values.firstOrNull() ?: throw IllegalArgumentException("invalid tree")
+            val root = TreeNode(rootValue)
+            val queue: Queue<TreeNode> = LinkedList()
+            queue.add(root)
+            var index = 1
+            while (queue.isNotEmpty()) {
+                val node = queue.remove()
+
+                node.left = values.getOrNull(index++)?.let { TreeNode((it)) }?.also { queue.add(it) }
+                node.right = values.getOrNull(index++)?.let { TreeNode((it)) }?.also { queue.add(it) }
+            }
+
+            return root
+        }
+    }
+}
---
+++ solutions/src/test/kotlin/com/bruce3x/leetcode/TreeNodeTest.kt
@@ -0,0 +1,61 @@
+package com.bruce3x.leetcode
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class TreeNodeTest {
+    @Test
+    fun testCreate() {
+        val root = TreeNode.create(1, 2, 3, null, null, 6, 7)
+        assertEquals(1, root.value)
+        assertEquals(2, root.left?.value)
+        assertEquals(3, root.right?.value)
+        assertEquals(null, root.left?.left?.value)
+        assertEquals(null, root.left?.right?.value)
+        assertEquals(6, root.right?.left?.value)
+        assertEquals(7, root.right?.right?.value)
+    }
+
+    @Test
+    fun testToString() {
+        assertEquals(
+            "[1, 2, 3, 4, 5, 6, 7, null, null, null, null, null, null, null, null]",
+            TreeNode.create(1, 2, 3, 4, 5, 6, 7).toString()
+        )
+
+        assertEquals(
+            "[1, 2, 3, null, null, 6, 7, null, null, null, null]",
+            TreeNode.create(1, 2, 3, null, null, 6, 7).toString()
+        )
+    }
+
+    @Test
+    fun testEquals() {
+        val node1 = TreeNode(1)
+        val node2 = TreeNode(2)
+        val node3 = TreeNode(3)
+        val node4 = TreeNode(4)
+        val node5 = TreeNode(5)
+        val node6 = TreeNode(6)
+        val node7 = TreeNode(7)
+
+        node1.left = node2
+        node1.right = node3
+        node2.left = null
+        node2.right = null
+        node3.left = node6
+        node3.right = node7
+
+        assertEquals(node1, TreeNode.create(1, 2, 3, null, null, 6, 7))
+
+        node2.left = node4
+        node2.right = node5
+
+        assertEquals(node1, TreeNode.create(1, 2, 3, 4, 5, 6, 7))
+
+        assertTrue(
+            TreeNode.create(1, 2, 3, null, 4, 5) == TreeNode.create(1, 2, 3, null, 4, 5)
+        )
+    }
+}

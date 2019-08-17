0d89185e3de7ed8304156c2b2dc7859f383eb1d9
bruce3x <im.brucezz@gmail.com>
2019-08-17 14:19:29 +0800

    optimize build script

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,11 +13,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("com.github.kittinunf.fuel:fuel:2.1.0")
-    implementation("com.github.kittinunf.fuel:fuel-json:2.1.0")
-    implementation("com.github.kittinunf.fuel:fuel-gson:2.1.0")
-    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.1.0")
+    implementation("com.github.kittinunf.fuel:fuel:2.2.0")
+    implementation("com.github.kittinunf.fuel:fuel-json:2.2.0")
+    implementation("com.github.kittinunf.fuel:fuel-gson:2.2.0")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.2.0")
     implementation("org.jsoup:jsoup:1.12.1")
+    implementation("org.json:json:20190722")
+    implementation("com.google.code.gson:gson:2.8.5")
 }
 
 tasks.withType<KotlinCompile> {
